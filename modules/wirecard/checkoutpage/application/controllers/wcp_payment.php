da2573f9c67590e8aa6b43bd69ba9d21d3ed42d3
jakubpolomsky <jakub.polomsky@partner.wirecard.com>
2017-08-29 11:01:48 +0200

    #8 add customerDeviceId
    - rename invoice icon as well

--- modules/wirecard/checkoutpage/application/controllers/wcp_payment.php
+++ modules/wirecard/checkoutpage/application/controllers/wcp_payment.php
@@ -50,6 +50,28 @@ class wcp_payment extends wcp_payment_parent
         return wdceepayment::isValidWCPPayment($paymethod);
     }
 
+    /**
+     * @return string
+     */
+    function getWcpRatePayConsumerDeviceId()
+    {
+        $config = oxRegistry::getConfig();
+
+        if(isset($_SESSION['wcs-consumerDeviceId'])) {
+            $consumerDeviceId = $_SESSION['wcs-consumerDeviceId'];
+        } else {
+            $timestamp = microtime();
+            $consumerDeviceId = md5($config->getConfigParam('sWcpCustomerId') . "_" . $timestamp);
+            $_SESSION['wcs-consumerDeviceId'] = $consumerDeviceId;
+        }
+        $ratepay = '<script language="JavaScript">var di = {t:"'.$consumerDeviceId.'",v:"WDWL",l:"Checkout"};</script>';
+        $ratepay .= '<script type="text/javascript" src="//d.ratepay.com/'.$consumerDeviceId.'/di.js"></script>';
+        $ratepay .= '<noscript><link rel="stylesheet" type="text/css" href="//d.ratepay.com/di.css?t='.$consumerDeviceId.'&v=WDWL&l=Checkout"></noscript>';
+        $ratepay .= '<object type="application/x-shockwave-flash" data="//d.ratepay.com/WDWL/c.swf" width="0" height="0"><param name="movie" value="//d.ratepay.com/WDWL/c.swf" /><param name="flashvars" value="t='.$consumerDeviceId.'&v=WDWL"/><param name="AllowScriptAccess" value="always"/></object>';
+
+        return $ratepay;
+    }
+
     public function getWcpPaymentLogo($paymethod)
     {
 
@@ -76,7 +98,7 @@ class wcp_payment extends wcp_payment_parent
             case 'wcp_installment':
                 return '<img src="' . $imgPath . 'installment.jpg" />';
             case 'wcp_invoice_b2b':
-                return '<img src="' . $imgPath . 'invoice.jpg" />';
+                return '<img src="' . $imgPath . 'invoice.png" />';
             case 'wcp_invoice_b2c':
                 return '<img src="' . $imgPath . 'invoice.png" />';
             case 'wcp_maestro':
